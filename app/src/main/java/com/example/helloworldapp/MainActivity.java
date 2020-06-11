db4ab26965dcf83159833f9743816cb97d2ffee7
truonghh99 <truonghh@beloit.edu>
2020-06-10 23:10:07 -0700

    First commit

---
+++ app/src/main/java/com/example/helloworldapp/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.helloworldapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((TextView) findViewById(R.id.textView)).setTextColor(getResources().getColor(R.color.colorPrimary));
+            }
+        });
+
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.rootView).setBackgroundColor(getResources().getColor(R.color.yellow));
+            }
+        });
+
+        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((TextView) findViewById(R.id.textView)).setText("Android is Awesome");
+            }
+        });
+    }
+
+
+
+
+}
